045258c0fbb41a2c894ad383d6d172bc73beb3c2
Daniel Salamon <salidani@gmail.com>
2016-08-05 10:36:38 +0200

    fixed log file name in server-start script

--- server-start.sh
+++ server-start.sh
@@ -4,4 +4,4 @@ export FLASK_APP=server.py
 rm nohup.last.out
 mv nohup.out nohup.last.out
 nohup python3 -m flask run --port 9876 --host 0.0.0.0 &
-echo "Tweetcool server started in background. Read the server.log file to read the output!"
+echo "Tweetcool server started in background. Read the nohup.out file to read the output!"
